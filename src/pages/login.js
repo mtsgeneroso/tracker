8e217e599372879c5603e33e3c7ed91bd81e64a0
Mateus Generoso <mtsgeneroso@gmail.com>
2020-07-03 20:57:56 -0300

    Fix indentation

--- src/pages/login.js
+++ src/pages/login.js
@@ -8,32 +8,31 @@ const LoginPage = () => (
     <div style={{
       padding: 10,
       display: 'flex',
-			flexDirection: 'column',
-			alignItems: 'center'
+      flexDirection: 'column',
+      alignItems: 'center'
     }}>
       <section style={{
         display: 'flex',
         gridTemplateColumns: '500px 2fr',
         gridTemplateRows: '40vh',
-				flexDirection: 'column',
+        flexDirection: 'column',
         gridRowGap: '10px',
         gridGap: '10px'
       }}>
         <h1>Log in</h1>
-				<form>
-					
-				<div>
-					<label>Email</label>
-					<input type="email"></input>
-				</div>
-				<div>
-					<label>Password</label>
-					<input type="password"></input>
-				</div>
-				<a href="">Forgot password</a>
-				<button type="submit">Log in</button>
-				<button type="button">Sign up</button>
-				</form>
+        <form>
+        <div>
+          <label>Email</label>
+          <input type="email"></input>
+        </div>
+        <div>
+          <label>Password</label>
+          <input type="password"></input>
+        </div>
+        <a href="">Forgot password</a>
+        <button type="submit">Log in</button>
+        <button type="button">Sign up</button>
+        </form>
       </section>
     </div>
   </Layout>
